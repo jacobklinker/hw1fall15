dc37233136ada667feb6a38d99d27261323608de
jacobklinker <jacob-klinker@uiowa.edu>
2015-09-05 00:45:11 +0000

    implementation

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,40 @@
-# When done, submit this entire file to the autograder.
-
-# Part 1
-
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arr.inject(0, &:+)
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  sum arr.sort.reverse.take(2)
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  arr.combination(2).each do |x,y|
+    return true if (x + y) == n
+  end
+  return false
 end
-
-# Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  s.downcase.match(/^(?=[^aeiou])(?=[a-z])/)
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  return s == '0' if s.length == 1
+  return s.match(/^[10]*00$/)
 end
-
-# Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize(isbn, price)
+    raise ArgumentError.new("Invalid params") if isbn.length == 0 || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    "$%0.2f" % @price
+  end
 end
